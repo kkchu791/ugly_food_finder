71d349cbec3ba6d6ad9e520ca416aeb570d70b5d
Kirk <kkchu791@gmail.com>
2016-03-04 03:05:40 -0800

    Add image persist ability for heroku

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,12 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',
+    :aws_access_key_id      => ENV["AWS_ACCESS_KEY"],
+    :aws_secret_access_key  => ENV["AWS_SECRET_KEY"]
+  }
+  config.fog_directory  = ENV["AWS_BUCKET"]
+end
