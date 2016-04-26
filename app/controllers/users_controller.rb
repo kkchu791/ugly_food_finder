6a278df484dabcfb2ee4b5789b7efc1e29622f6b
Kirk <kkchu791@gmail.com>
2016-04-25 19:59:52 -0700

    Remove unused files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_url, notice: "Signed up!"
+      redirect_to markets_url, notice: "Signed up!"
     else
       render "new"
     end
